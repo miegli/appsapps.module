0a52fbcbfbb74fcc20aaee50229decc1e48d3d37
miegli <michae.egli@phlu.ch>
2017-11-09 09:46:24 +0100

    prepare for first release

--- src/appsapp-module.module.ts
+++ src/appsapp-module.module.ts
@@ -1,14 +1,45 @@
-import { Observable } from 'rxjs';
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { AppsappModuleProvider} from "./providers/appsapp-module-provider";
+import {Observable} from 'rxjs';
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import {AppsappModuleProvider} from "./providers/appsapp-module-provider";
+import {AppsappInputComponent} from "./components/input/appsapp-input/appsapp-input";
+import {AppsappInputUrlComponent} from "./components/input/appsapp-input-url/appsapp-input-url";
+import {AppsappInputNumberComponent} from "./components/input/appsapp-input-number/appsapp-input-number";
+import {AppsappInputTextareaComponent} from "./components/input/appsapp-input-textarea/appsapp-input-textarea";
+import {AppsappInputTelComponent} from "./components/input/appsapp-input-tel/appsapp-input-tel";
+import {AppsappInputEmailComponent} from "./components/input/appsapp-input-email/appsapp-input-email";
+import {AppsappInputDatesComponent} from "./components/input/appsapp-input-dates/appsapp-input-dates";
+import {AppsappInputDateComponent} from "./components/input/appsapp-input-date/appsapp-input-date";
+import {AppsappInputPasswordComponent} from "./components/input/appsapp-input-password/appsapp-input-password";
+import {AppsappInputIntegerComponent} from "./components/input/appsapp-input-integer/appsapp-input-integer";
+import {AppsappInputBooleanComponent} from "./components/input/appsapp-input-boolean/appsapp-input-boolean";
+import {AppsappInputRatingComponent} from "./components/input/appsapp-input-rating/appsapp-input-rating";
+import {AppsappInputBirthdayComponent} from "./components/input/appsapp-input-birthday/appsapp-input-birthday";
+import {AppsappInputTextComponent} from "./components/input/appsapp-input-text/appsapp-input-text";
+import {AppsappInputAbstractComponent} from "./components/input/appsapp-input-abstract";
 
 @NgModule({
-  declarations: [
-    // declare all components that your module uses
-  ],
-  exports: [
-    // export the component(s) that you want others to be able to use
-  ]
+    declarations: [
+        // declare all components that your module uses
+        AppsappInputComponent,
+        AppsappInputUrlComponent,
+        AppsappInputNumberComponent,
+        AppsappInputTextareaComponent,
+        AppsappInputTelComponent,
+        AppsappInputEmailComponent,
+        AppsappInputDatesComponent,
+        AppsappInputDateComponent,
+        AppsappInputPasswordComponent,
+        AppsappInputIntegerComponent,
+        AppsappInputBooleanComponent,
+        AppsappInputRatingComponent,
+        AppsappInputBirthdayComponent,
+        AppsappInputTextComponent,
+        AppsappInputAbstractComponent
+    ],
+    exports: [
+        // export the component(s) that you want others to be able to use
+        AppsappInputComponent
+    ]
 })
 export class AppsappModule {
     static initializeApp(config: {
