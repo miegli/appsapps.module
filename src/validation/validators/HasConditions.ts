c436b28e8ffbc1788b012d4c53cfec3e8c6429e0
miegli <michae.egli@phlu.ch>
2017-11-10 15:28:26 +0100

    added circular validation of conditions

--- src/validation/validators/HasConditions.ts
+++ src/validation/validators/HasConditions.ts
@@ -3,110 +3,72 @@ import {Observable} from "rxjs/Observable";
 import {Validator} from "class-validator";
 
 export function HasConditions(options: [{
-    value?: any,
-    property?: string,
-    operand?: any,
-    validator?: 'equals' | 'notEquals' | 'minLength' | 'maxLength' | 'matches' | 'isEmpty' | 'isNotEmpty' | 'isIn' | 'isNotIn' | 'isBoolean' | 'isDate' | 'isString' | 'isArray' | 'isNumber' | 'isInt' | 'isEnum' | 'isDivisibleBy' | 'isPositive' | 'isNegative' | 'max' | 'min' | 'minDate' | 'maxDate' | 'contains' | 'notContains' | 'isAlpha' | 'isAlphanumeric' | 'isAscii' | 'isBase64' | 'isCreditCard' | 'isCurrency' | 'isEmail' | 'isFQDN' | 'isHexColor' | 'isHexadecimal' | 'isIP' | 'isISBN' | 'isISIN' | 'isISO8601' | 'isJSON' | 'isLowercase' | 'isMobilePhone' | 'isURL' | 'isUUID' | 'isUppercase' | 'length' | 'isMilitaryTime' | 'arrayContains' | 'arrayNotContains' | 'arrayNotEmpty' | 'arrayMinSize' | 'arrayMaxSize' | 'arrayUnique',
-    type?: 'condition' | 'limit'
-    validatorAdditionalArgument?: any
+  value?: any,
+  property?: string,
+  operand?: any,
+  validator?: 'equals' | 'notEquals' | 'minLength' | 'maxLength' | 'matches' | 'isEmpty' | 'isNotEmpty' | 'isIn' | 'isNotIn' | 'isBoolean' | 'isDate' | 'isString' | 'isArray' | 'isNumber' | 'isInt' | 'isEnum' | 'isDivisibleBy' | 'isPositive' | 'isNegative' | 'max' | 'min' | 'minDate' | 'maxDate' | 'contains' | 'notContains' | 'isAlpha' | 'isAlphanumeric' | 'isAscii' | 'isBase64' | 'isCreditCard' | 'isCurrency' | 'isEmail' | 'isFQDN' | 'isHexColor' | 'isHexadecimal' | 'isIP' | 'isISBN' | 'isISIN' | 'isISO8601' | 'isJSON' | 'isLowercase' | 'isMobilePhone' | 'isURL' | 'isUUID' | 'isUppercase' | 'length' | 'isMilitaryTime' | 'arrayContains' | 'arrayNotContains' | 'arrayNotEmpty' | 'arrayMinSize' | 'arrayMaxSize' | 'arrayUnique',
+  type?: 'condition' | 'limit'
+  validatorAdditionalArgument?: any
 }], actionIfMatches?: 'hide' | 'show', validationOptions?: ValidationOptions) {
-    return function (object: Object, propertyName: string) {
+  return function (object: Object, propertyName: string) {
 
 
-        let self = this;
+    let self = this;
 
-        if (actionIfMatches == undefined) {
-            actionIfMatches = 'show';
-        }
-
-
-        options.forEach((option) => {
-
-            if (option.property == undefined) {
-                option.property = propertyName;
-            }
-            if (option.validator == undefined) {
-                option.validator = 'equals';
-            }
-            if (option.type == undefined) {
-                option.type = 'condition';
-            }
-            if (option.value == undefined) {
-                option.value = true;
-            }
-        });
+    if (actionIfMatches == undefined) {
+      actionIfMatches = 'show';
+    }
 
 
-        registerDecorator({
-            name: "hasConditions",
-            target: object.constructor,
-            propertyName: propertyName,
-            constraints: [{'type': 'hasConditions', 'value': options, 'actionIfMatches': actionIfMatches}],
-            options: {groups: ['condition_' + propertyName]},
-            validator: {
-                validate(value: any, args: any) {
+    options.forEach((option) => {
 
-                    const validator = new Validator();
-
-                    return new Promise(function (resolve, reject) {
-
-
-                        let observerable = new Observable((observer) => {
-
-                            /**
-                             * apply calculated rule
-                             * @param state
-                             */
-                            const applyRule = (state) => {
-                                if (state) {
-                                    observer.next(); // if true
-                                } else {
-                                    resolve(false); // if false
-                                }
-                            }
+      if (option.property == undefined) {
+        option.property = propertyName;
+      }
+      if (option.validator == undefined) {
+        option.validator = 'equals';
+      }
+      if (option.type == undefined) {
+        option.type = 'condition';
+      }
+      if (option.value == undefined) {
+        option.value = true;
+      }
+    });
 
 
-                            /**
-                             * iterates over all rules
-                             */
-                            if (options) {
-                                options.forEach((condition: any) => {
+    registerDecorator({
+      name: "hasConditions",
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [{'type': 'hasConditions', 'value': options, 'actionIfMatches': actionIfMatches}],
+      options: {groups: ['condition_' + propertyName]},
+      validator: {
+        validate(value: any, args: any) {
 
-                                    switch (condition.type) {
-                                        case 'condition':
-                                            applyRule(validator[condition.validator](args.object.__conditionContraintsPropertiesValue[condition.property] === undefined ? args.object[condition.property] : args.object.__conditionContraintsPropertiesValue[condition.property], condition.value, condition.validatorAdditionalArgument));
-                                            break;
+          const validator = new Validator();
 
-                                        default:
-                                            observer.next();
-                                    }
-
-
-                                });
-                            } else {
-                                resolve(true);
-                            }
-                        });
-
-                        /**
-                         * apply rules result
-                         */
-                        let calculated = 0;
-                        observerable.subscribe(() => {
-                            calculated++;
-                            if (calculated >= options.length) {
-                                resolve(true);
-                            }
-                        });
-
-
-                    });
-
-
+          let state = true;
+          /**
+           * iterates over all rules synchronous
+           */
+          if (options) {
+            options.forEach((condition: any) => {
+              if (condition.type == 'condition') {
+                if (!validator[condition.validator](args.object.__conditionContraintsPropertiesValue[condition.property] === undefined ? args.object[condition.property] : args.object.__conditionContraintsPropertiesValue[condition.property], condition.value, condition.validatorAdditionalArgument)) {
+                  state = false;
+                  return state;
                 }
-            }
-        });
+              }
+            });
+          }
+
+          return state;
+
+        }
+      }
+    });
 
 
-    };
+  };
 }
